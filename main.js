9e1def7c170547b0aa4d95e450d5dee4d8f4a1a2
Fafowora Sunkanmi <sunkanmifafowora@gmail.com>
2025-12-03 16:26:34 +0100

    progressive enhancement added using .default CSS class

--- main.js
+++ main.js
@@ -6,28 +6,32 @@
     });
     return;
   }
-  console.log("ViewTransition NOT supported — using JS fallback");
+  
+  document.querySelectorAll(".default").forEach(el => {
+    el.classList.remove("default");
+  });
+  
+  // console.log("ViewTransition NOT supported — using JS fallback");
   const allAnchors = document.querySelectorAll("a"); 
   
   allAnchors.forEach(anchor => {
     anchor.addEventListener("click", function (e) {
-      const link = e.target; e.preventDefault(); // animate current page OUT 
-      console.log(document.documentElement); // let animation begin, then navigate 
+    
+      e.preventDefault(); // animate current page OUT 
+
+      // console.log(document.documentElement); // let animation begin, then navigate 
       document.documentElement.classList.add("leaving");
 
-      setTimeout(() => { window.location.href = link.href; }, 300);
+      setTimeout(() => { window.location.href = anchor.href; }, 300);
     });
   });
+  
   // outgoing animation on link click // incoming animation (DOMContentLoaded + rAF) 
   function reveal() {
     const els = document.querySelectorAll(".reveal");
     requestAnimationFrame(() => { els.forEach(el => el.classList.add("visible")); });
   }
-  if (didViewTransition) {
-    console.log('skipping reveal due to VT');
-    return
-  }
-  else {
+  
     document.addEventListener("DOMContentLoaded", reveal, { once: true });
-  }
+  
 })();
